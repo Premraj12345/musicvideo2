826dd032e11d439c68681bef3a7f57bf7a7d041e
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2023-02-10 02:33:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import os
+from download import download
+from time import sleep
+
+st.header('App') 
+
+url = 'https://checker.in/go/4594322'
+youtube_ffmpeg_command = 'ffmpeg -re  -stream_loop -1 -i video.mp4 -c copy output.mp4 -c:v libx264 -c:a aac -f flv -b:v 750k -g 48 -keyint_min 48 -sc_threshold 0 rtmp://a.rtmp.youtube.com/live2/egr2-jwmx-pshz-e1g8-cc6k'
+
+status = download(url,'video.mp4')
+st.markdown(status)
+
+sleep(100)
+
+text = os.popen(youtube_ffmpeg_command).read()
+st.markdown(text)
+
+st.text("Working")
