e8be43d3df2852b226731f85bdce713305585fe5
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2023-02-10 15:04:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import os
 from download import download
 from time import sleep
 
+
 st.header('App') 
 
+VBR="1500k"
+FPS="24"
+QUAL="superfast"
+
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
+KEY="egr2-jwmx-pshz-e1g8-cc6k"
+VIDEO_SOURCE="video.mp4"
+
 url = 'https://checker.in/go/4639475'
-youtube_ffmpeg_command = 'script.sh "rtmp://a.rtmp.youtube.com/live2" "egr2-jwmx-pshz-e1g8-cc6k" "video.mp4"'
+youtube_ffmpeg_command = f'ffmpeg -stream_loop -1 -re -i {VIDEO_SOURCE} -c:v libx264 -pix_fmt yuv420p -preset {QUAL} -r {FPS} -g 48 -b:v {VBR} -c:a aac -f flv {YOUTUBE_URL}/{KEY}'
 
 status = download(url,'video.mp4')
 st.markdown(status)
